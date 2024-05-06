3a097a2963e88b70a1ff38fc65c9483efdcac921
Stanislaw Malewicz <szeliga71@wp.pl>
2024-05-06 21:40:10 +0200

    06.05.24

--- src/test/java/DeckApiHandlerTest.java
+++ src/test/java/DeckApiHandlerTest.java
@@ -9,55 +9,59 @@ import java.net.http.HttpClient;
 public class DeckApiHandlerTest {
 
 
-
-
     private final HttpClient client = HttpClient.newHttpClient();
     DeckApiHandler handler = new DeckApiHandler();
 
-   @Test
+    @Test
     void getDeckHappyPath() {
         Assertions.assertNotNull(handler.getShuffledDecks(2));
     }
+
     @Test
     void getDecksHappyPath1() {
-        Assertions.assertEquals(200,handler.getShuffledDecks(2).statusCode());
+        Assertions.assertEquals(200, handler.getShuffledDecks(2).statusCode());
     }
+
     @Test
     void getDecksArgumentZero() {
 
-       Assertions.assertThrows(IllegalArgumentException.class,()->handler.getShuffledDecks(0));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> handler.getShuffledDecks(0));
     }
+
     @Test
     void getDecksnvalidArgumentNegativeValue() {
 
-        Assertions.assertThrows(IllegalArgumentException.class,()->handler.getShuffledDecks(-5));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> handler.getShuffledDecks(-5));
     }
+
     @Test
     void getDecksnvalidArgumentTooBigValue() {
 
-        Assertions.assertThrows(IllegalArgumentException.class,()->handler.getShuffledDecks(100));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> handler.getShuffledDecks(100));
     }
 
     @Test
-    void drawCardsIncorrectFirstArgument(){
+    void drawCardsIncorrectFirstArgument() {
 
-       Assertions.assertEquals(404,handler.drawCards("xxxx",2).statusCode());
-    }
-    @Test
-    void drawCardsIncorrectBothArguments(){
-
-        Assertions.assertEquals(404,handler.drawCards("xxxx",0).statusCode());
+        Assertions.assertEquals(404, handler.drawCards("xxxx", 2).statusCode());
     }
 
     @Test
-    void drawCardsIncorrecFirstArgument1(){
+    void drawCardsIncorrectBothArguments() {
 
-        Assertions.assertThrows(IllegalArgumentException.class,()->handler.drawCards("",1));
+        Assertions.assertEquals(404, handler.drawCards("xxxx", 0).statusCode());
     }
-    @Test
-    void drawCardsIncorrectFirstArgument2(){
 
-        Assertions.assertThrows(IllegalArgumentException.class,()->handler.drawCards(null,2));
+    @Test
+    void drawCardsIncorrecFirstArgument1() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> handler.drawCards("", 1));
+    }
+
+    @Test
+    void drawCardsIncorrectFirstArgument2() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> handler.drawCards(null, 2));
     }
 }
 
