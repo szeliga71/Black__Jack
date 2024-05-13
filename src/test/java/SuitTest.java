d8c7b7e0a2be827a6eb637c7dece2a29a5d0f087
Stanislaw Malewicz <szeliga71@wp.pl>
2024-05-13 11:45:37 +0200

    testss 13.05.24 nr 1

--- src/test/java/SuitTest.java
+++ src/test/java/SuitTest.java
@@ -4,31 +4,37 @@ import org.junit.jupiter.api.Test;
 
 public class SuitTest {
     @Test
-    void fromStringToSuitHappyHearts(){
+    void fromStringToSuitHappyHearts() {
         Assertions.assertEquals(Suit.HEARTS, Suit.valueOf("HEARTS"));
     }
+
     @Test
-    void fromStringToSuitHappyClubs(){
+    void fromStringToSuitHappyClubs() {
         Assertions.assertEquals(Suit.CLUBS, Suit.valueOf("CLUBS"));
     }
+
     @Test
-    void fromStringToSuitHappySpades(){
+    void fromStringToSuitHappySpades() {
         Assertions.assertEquals(Suit.SPADES, Suit.valueOf("SPADES"));
     }
+
     @Test
-    void fromStringToSuitHappyDiamonds(){
+    void fromStringToSuitHappyDiamonds() {
         Assertions.assertEquals(Suit.DIAMONDS, Suit.valueOf("DIAMONDS"));
     }
+
     @Test
-    void fromStringToSuitInvalidInput(){
-        String illegalText="xxxxxxx";
+    void fromStringToSuitInvalidInput() {
+        String illegalText = "xxxxxxx";
         Assertions.assertThrows(IllegalArgumentException.class, () -> Suit.fromStringtoSuit(illegalText));
     }
+
     @Test
-    void fromStringToSuitEmptyInput(){
-        String illegalText="";
+    void fromStringToSuitEmptyInput() {
+        String illegalText = "";
         Assertions.assertThrows(IllegalArgumentException.class, () -> Suit.fromStringtoSuit(illegalText));
     }
+
     @Test
     void fromStringToSuitInvalidInput1() {
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> Suit.fromStringtoSuit("invalid"));
